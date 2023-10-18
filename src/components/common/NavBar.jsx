98ecc51e87af496a36b80df951e459cca199c72d
ishanksainger <isainger29@gmail.com>
2023-10-18 22:55:51 +0530

    update navbar

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -20,6 +20,7 @@ function Navbar() {
   const [subLinks, setSubLinks] = useState([]);
   const [loading, setLoading] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false); // Track the menu open state
+  const [isCatalogMenuOpen, setIsCatalogMenuOpen] = useState(false);
 
   const fetchSubLinks = async () => {
     setLoading(true);
@@ -41,85 +42,106 @@ function Navbar() {
   };
   const closeMenu = () => {
     setIsMenuOpen(false);
+    setIsCatalogMenuOpen(false);
   };
-
+  const toggleCatalogMenu = (e) => {
+    e.stopPropagation();
+    setIsCatalogMenuOpen(!isCatalogMenuOpen);
+  };
   return (
     <div
-      className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${location.pathname !== "/" ? "bg-richblack-800" : ""} transition-all duration-200`}
+      className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
+        location.pathname !== "/" ? "bg-richblack-800" : ""
+      } transition-all duration-200`}
     >
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
         <Link to="/">
-          <img src={logo} alt="Logo"  loading="lazy" className=" w-[110px] sm:w-[160px] sm:h-[32px]" />
+          <img
+            src={logo}
+            alt="Logo"
+            loading="lazy"
+            className=" w-[110px] sm:w-[160px] sm:h-[32px]"
+          />
         </Link>
 
         {/* Mobile Menu Icon */}
         <div className="flex">
           <button
-            className=' mr-1 md:hidden'
+            className=" mr-1 md:hidden"
             onClick={() => setIsMenuOpen(!isMenuOpen)} // Toggle the menu when the button is clicked
           >
             <AiOutlineMenu fontSize={24} fill="#AFB2BF" />
           </button>
           {/* Show Login and Signup buttons on mobile screens when user is logged out */}
           <div className="flex items-center gap-x-2 sm:gap-x-4 md:hidden">
-          <div className="flex gap-x-3 items-center">
-          {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
-            <Link to="/dashboard/cart" className="relative">
-              <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
-              {totalItems > 0 && (
-                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
-                  {totalItems}
-                </span>
+            <div className="flex gap-x-3 items-center">
+              {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
+                <Link to="/dashboard/cart" className="relative">
+                  <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
+                  {totalItems > 0 && (
+                    <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                      {totalItems}
+                    </span>
+                  )}
+                </Link>
               )}
-            </Link>
-          )}
-          {token !== null && <div className="md:hidden relative flex items-center">
-            <ProfileDropdown />
-            </div>}
+              {token !== null && (
+                <div className="md:hidden relative flex items-center">
+                  <ProfileDropdown />
+                </div>
+              )}
             </div>
             {token === null && (
               <Link to="/login">
-                <button className="rounded-[8px]  border border-richblack-700 bg-richblack-800 px-[8px] py-[4px] sm:px-[12px] sm:py-[8px] text-richblack-100">
+                <button
+                  className="rounded-[8px]  border border-richblack-700 bg-richblack-800 px-[8px] py-[4px] sm:px-[12px] sm:py-[8px] text-richblack-100"
+                  onClick={closeMenu}
+                >
                   Log in
                 </button>
               </Link>
             )}
             {token === null && (
               <Link to="/signup">
-                <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[8px] py-[4px] sm:px-[12px] sm:py-[8px] text-richblack-100">
+                <button
+                  className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[8px] py-[4px] sm:px-[12px] sm:py-[8px] text-richblack-100"
+                  onClick={closeMenu}
+                >
                   Sign up
                 </button>
               </Link>
             )}
           </div>
-          
-
         </div>
         {/* Navigation links for small screens */}
         {isMenuOpen && (
           <div
-            className={`md:hidden fixed z-10 top-14 left-0 right-0 bg-richblack-800 py-2 transform ${isMenuOpen ? 'mobile-menu-animation' : ''} transition-transform duration-300`}
+            className={`md:hidden fixed z-50 top-14 left-0 right-0 bg-richblack-800 py-2 transform ${
+              isMenuOpen ? "mobile-menu-animation" : ""
+            } transition-transform duration-300`}
             onClick={closeMenu}
             onMouseLeave={closeMenu}
           >
-
             <ul className="flex flex-col items-center gap-4">
               {NavbarLinks.map((link, index) => (
                 <li key={index}>
                   {link.title === "Catalog" ? (
                     <div
                       className={`group relative flex cursor-pointer items-center gap-1 text-richblack-25`}
-                    >
+                      onClick={(e) => 
+                        toggleCatalogMenu(e)}>
                       <p>{link.title}</p>
                       <BsChevronDown />
-                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] 
-                      flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg 
-                      bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all 
-                      duration-150 lg:w-[300px] group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100"
-                        onMouseLeave={closeMenu}>
+                      <div
+                        className={`absolute left-[50%] top-0 z-50 w-[300px] translate-x-[-50%]
+                      flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 ${
+                        isCatalogMenuOpen
+                          ? "visible opacity-100 translate-y-[1.65em] "
+                          : "invisible opacity-0"
+                      } transition-all duration-150`}
+                      >
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
 
-                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] 
-                        translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
                         {loading ? (
                           <p className="text-center">Loading...</p>
                         ) : subLinks.length ? (
@@ -128,7 +150,10 @@ function Navbar() {
                               ?.filter((subLink) => subLink?.course?.length > 0)
                               ?.map((subLink, i) => (
                                 <Link
-                                  to={`/catalog/${subLink.name.split(" ").join("-").toLowerCase()}`}
+                                  to={`/catalog/${subLink.name
+                                    .split(" ")
+                                    .join("-")
+                                    .toLowerCase()}`}
                                   className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
                                   key={i}
                                 >
@@ -142,7 +167,7 @@ function Navbar() {
                       </div>
                     </div>
                   ) : (
-                    <Link to={link?.path}>
+                    <Link to={link?.path} onClick={closeMenu}>
                       <p className="text-richblack-25">{link.title}</p>
                     </Link>
                   )}
@@ -159,13 +184,20 @@ function Navbar() {
               <li key={index}>
                 {link.title === "Catalog" ? (
                   <div
-                    className={`group relative flex cursor-pointer items-center gap-1 ${matchRoute("/catalog/:catalogName") ? "text-yellow-25" : "text-richblack-25"}`}
+                    className={`group relative flex cursor-pointer items-center gap-1 ${
+                      matchRoute("/catalog/:catalogName")
+                        ? "text-yellow-25"
+                        : "text-richblack-25"
+                    }`}
                   >
                     <p>{link.title}</p>
                     <BsChevronDown />
-                    <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] 
-                    translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all 
-                    duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                    <div
+                      className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%]
+                    translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all
+
+                    duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]"
+                    >
                       <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
                       {loading ? (
                         <p className="text-center">Loading...</p>
@@ -175,7 +207,10 @@ function Navbar() {
                             ?.filter((subLink) => subLink?.course?.length > 0)
                             ?.map((subLink, i) => (
                               <Link
-                                to={`/catalog/${subLink.name.split(" ").join("-").toLowerCase()}`}
+                                to={`/catalog/${subLink.name
+                                  .split(" ")
+                                  .join("-")
+                                  .toLowerCase()}`}
                                 className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
                                 key={i}
                               >
@@ -190,7 +225,13 @@ function Navbar() {
                   </div>
                 ) : (
                   <Link to={link?.path}>
-                    <p className={`${matchRoute(link?.path) ? "text-yellow-25" : "text-richblack-25"}`}>
+                    <p
+                      className={`${
+                        matchRoute(link?.path)
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
                       {link.title}
                     </p>
                   </Link>
@@ -214,14 +255,20 @@ function Navbar() {
           )}
           {token === null && (
             <Link to="/login">
-              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+              <button
+                className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100"
+                onClick={closeMenu}
+              >
                 Log in
               </button>
             </Link>
           )}
           {token === null && (
             <Link to="/signup">
-              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+              <button
+                className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100"
+                onClick={closeMenu}
+              >
                 Sign up
               </button>
             </Link>
